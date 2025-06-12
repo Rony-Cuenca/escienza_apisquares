3fdc2091bfd465bcf95aa7685e4eedf39093f872
rony-cuenca <jrony.cr@gmail.com>
2025-06-12 18:03:36 -0500

    DETAILS IN THE DESIGN OF THE NAVBAR AND FOOT

--- src/view/layout.php
+++ src/view/layout.php
@@ -4,9 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/escienza_apisquares/assets/css/output.css" rel="stylesheet">
+  <!-- <link href="/escienza_apisquares/assets/css/output.css" rel="stylesheet"> -->
   <title>Escienza - Cuadres</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link href="../assets/css/output.css?v=<?php echo time(); ?>" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 </head>
 
 <body class="min-h-screen flex flex-col">
@@ -21,6 +23,8 @@
   <?php if (isset($_SESSION['id_cliente'])): ?>
     <?php require 'view/ui/footer.php'; ?>
   <?php endif; ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 
 </html>
--- src/view/ui/footer.php
+++ src/view/ui/footer.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
 <body>
-    <footer class="bg-white shadow-[0_-4px_4px_0_rgba(0,0,0,0.25)] w-full h-[60px] flex items-center justify-center z-40">
-        <span class="font-light  text-[18px] text-center tracking-[0.15px] w-full">
+    <footer class="bg-slate-100 w-full h-auto flex items-center justify-center z-40">
+        <span class="font-sans  text-lg text-center tracking-[0.15px] w-full">
             Copyright &copy; 2025 ESCIENZA. Todos los derechos reservados.<br>
             Made by <a href="https://escienza.pe/" target="_blank">Escienza</a>
         </span>
     </footer>
 </body>
-
-</html>
